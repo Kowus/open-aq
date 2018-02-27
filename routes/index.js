986f42a4795c932f7742428b8edb5c32ec50870f
Kowus <barnabas@elite-education.org>
2018-02-27 05:06:54 +0000

    configure signup route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express'),
+  router = express.Router(),
+  passport = require('passport')
+  ;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/profile', function (req, res, next) {
+  res.redirect('/');
+});
+router.post('/signup', isNotLoggedIn, passport.authenticate('local-signup', {
+  successRedirect: '/profile',
+  failureRedirect: '/signup',
+  failureFlash: true
+}));
+
 
 module.exports = router;
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
+function isNotLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+    res.redirect('/profile');
+  else
+    return next();
+}
+
+function needsGroup(group) {
+  return function (req, res, next) {
+    if (req.user && req.user.group === group) {
+      res.locals.user = req.user;
+      next();
+    }
+    else {
+      req.session.message = "Unauthorized Access";
+      res.status(401).redirect(`/login?next=${req.originalUrl}`);
+    }
+  };
+}
