d599def4d1f28e026f158523d77c452aede382be
Kowus <barnabas@elite-education.org>
2018-02-27 04:33:09 +0000

    async/await local signup strategy resolves #3

--- config/passport.js
+++ config/passport.js
@@ -14,14 +14,63 @@ module.exports = passport => {
         });
     });
 
+    passport.use('local-signup', new LocalStrategy({
+        usernameField: 'username',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, async (req, username, password, done) => {
+        let userEmail,
+            userUsname
+            ;
+        try {
+            userEmail = await User.findOne({ email: req.body.email });
+            userUsname = await User.findOne({ username: username })
+            if (userEmail) return done(null, false, req.flash('Sorry, that email has already been used with an account.'));
+            else if (userUsname) return done(null, false, req.flash('Sorry, that username has already been used with an account.'));
+            else {
+                let newUser = new User({
+                    username: username,
+                    given_name: req.body.given_name,
+                    family_name: req.body.family_name,
+                    email: req.body.email,
+                    password: req.body.password
+                });
+                let user;
+                try {
+                    user = await newUser.save();
+                    // res.json(user)
+                    let token;
+                    try {
+                        token = await jwt.sign({
+                            user: user
+                        }, env.jwt.key, { audience: env.jwt.audience, issuer: env.jwt.issuer });
+                        // Use token to send mail confirmation
+                        // res.json(token);
+                        return done(null, user);
+                    } catch (error) {
+                        console.error(error);
+                        return done(error);
+                    }
+
+                } catch (err) {
+                    console.error(error);
+                    return done(error);
+                }
+            }
+        } catch (error) {
+            console.error(error);
+            return done(error);
+        }
+    }))
+
     passport.use('local-login', new LocalStrategy({
-        usernameField: username,
-        passwordField: password,
+        usernameField: 'username',
+        passwordField: 'password',
         passReqToCallback: true
     }, async (req, username, password, done) => {
         let user;
         try {
-            user = await User.findOne({ email: req.body.username.toLowerCase() }) || await User.findOne({ username: req.body.username.toLowerCase() });
+            user = await User.findOne({ email: username.toLowerCase() }) || await User.findOne({ username: username.toLowerCase() });
             if (!user) {
                 return done(null, false, req.flash('loginMessage', 'No user found'));
             } else {
