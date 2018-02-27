b40d6787a803afe1d69f13ba036b7f78527b3f8a
Kowus <barnabas@elite-education.org>
2018-02-27 04:11:12 +0000

    async/await login implementation for passport local strategy

--- config/passport.js
+++ config/passport.js
@@ -13,4 +13,35 @@ module.exports = passport => {
             done(err, user);
         });
     });
+
+    passport.use('local-login', new LocalStrategy({
+        usernameField: username,
+        passwordField: password,
+        passReqToCallback: true
+    }, async (req, username, password, done) => {
+        let user;
+        try {
+            user = await User.findOne({ email: req.body.username.toLowerCase() }) || await User.findOne({ username: req.body.username.toLowerCase() });
+            if (!user) {
+                return done(null, false, req.flash('loginMessage', 'No user found'));
+            } else {
+                let isMatch;
+                try {
+                    isMatch = user.comparePassword(req.body.password);
+                    if (isMatch) {
+                        return done(null, user)
+                    } else {
+                        return done(null, false, req.flash('loginMessage', 'Email or Password incorrect'));
+                    }
+                } catch (error) {
+                    console.error(error)
+                    return done(null, false, req.flash('loginMessage', 'Email or Password incorrect'));
+
+                }
+            }
+        } catch (err) {
+            console.error(err);
+            return done(err);
+        }
+    }))
 }
