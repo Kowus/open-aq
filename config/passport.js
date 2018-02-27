4ea9f2ea647d123743fe5f548060a9fd3bd52848
Kowus <barnabas@elite-education.org>
2018-02-27 03:38:07 +0000

    serialize/deserialize user ref: #4

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,16 @@
-const LocalStrategy = require('passport-local'),
+const LocalStrategy = require('passport-local').Strategy,
     env = require('../lib/env')
     ;
+let User = require('../models/user');
+
+module.exports = passport => {
+    passport.serializeUser(function (req, user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function (req, id, done) {
+        User.findById(id, function (err, user) {
+            done(err, user);
+        });
+    });
+}
